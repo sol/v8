8ba779262cc89d48c33eb851e0d8ae10c488e363
Simon Hengel <sol@typeful.net>
2012-11-17 11:46:01 +0100

    More tests

--- test/Foreign/JavaScript/V8/ContextSpec.hs
+++ test/Foreign/JavaScript/V8/ContextSpec.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Foreign.JavaScript.V8.ContextSpec (main, spec) where
 
 import           Test.Hspec
+import           Data.String.Builder (build)
 
 import           Control.Exception (bracket)
 
@@ -29,6 +31,39 @@ spec = do
           t <- mkObjectTemplate
           contextNew t >>= dispose
           dispose t `shouldThrow` (== AlreadyDisposed)
+
+    it "can share values with other contexts" $ do
+      withHandleScope $ do
+        c1 <- mkObjectTemplate >>= contextNew
+        v <- withContextScope c1 $ do
+          runScript "'bar'"
+
+        t <- mkObjectTemplate
+        objectTemplateAddFunction t "foo" $ \_ -> do
+          return v
+
+        bracket (contextNew t) dispose $ \c2 -> do
+          withContextScope c2 $ do
+            (runScript "foo()" >>= toString) `shouldReturn` "bar"
+        dispose c1
+
+    it "can share objects with other contexts" $ do
+      withHandleScope $ do
+        c1 <- mkObjectTemplate >>= contextNew
+        v <- withContextScope c1 $ do
+          runScript . build $ do
+            "var foo = new Object()"
+            "foo.bar = 23"
+            "foo"
+
+        t <- mkObjectTemplate
+        objectTemplateAddFunction t "foo" $ \_ -> do
+          return v
+
+        bracket (contextNew t) dispose $ \c2 -> do
+          withContextScope c2 $ do
+            (runScript "foo().bar" >>= toString) `shouldReturn` "23"
+        dispose c1
   where
     jsReverse args = do
       s <- argumentsGet 0 args >>= toString
